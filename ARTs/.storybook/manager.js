0f85c38550c7557976d8ba99895845c418b798d4
thnbcu <timothy.hunold@nbcuni.com>
2024-10-17 10:59:48 -0700

    :lipstick: NBCU theme starter

--- ARTs/.storybook/manager.js
+++ ARTs/.storybook/manager.js
@@ -1,7 +1,8 @@
 import { addons } from '@storybook/manager-api';
 import { themes } from '@storybook/theming';
-import NBCU from './nbcutheme';
+import nbcu from './nbcutheme';
 
 addons.setConfig({
   theme: themes.dark,
+  //theme: nbcu,
 });
